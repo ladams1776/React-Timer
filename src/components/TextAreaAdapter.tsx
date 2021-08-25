da3dac78d81ea0a8a21ea49c72fc96db239c0924
la <ladams1776@yahoo.com>
2021-08-24 23:55:44 -0400

    Adjusting the textarea based on size of window

--- src/components/TextAreaAdapter.tsx
+++ src/components/TextAreaAdapter.tsx
@@ -16,22 +16,38 @@ interface TextAreaAdapterProp {
 export const Adapter: React.FC<AdapterProp> = ({ ...rest }) => {
   const { onChange, value } = rest.input;
 
-  const toolbar = 'fullscreen | undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
-  const plugins = ['advlist autolink lists link image charmap print preview anchor', 'searchreplace visualblocks code fullscreen', 'insertdatetime media table paste code help wordcount'];
+  const toolbar =
+    'fullscreen | undo redo | formatselect | bold italic backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help';
+  const plugins = [
+    'advlist autolink lists link image charmap print preview anchor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table paste code help wordcount',
+  ];
 
+  const [screenWidth, setScreenWidth] = React.useState(window.innerWidth);
+
+  const init = screenWidth < 600
+    ? {
+        height: '95vw',
+        width: '100vw',
+        menubar: true,
+        plugins,
+        toolbar,
+      }
+    : {
+        height: '80vw',
+        width: '100%',
+        menubar: true,
+        plugins,
+        toolbar,
+      }; 
 
   return (
     <Editor
       data-test-id="text-area-adapter"
       test-dataid="textAreaAdapter"
       value={value}
-      init={{
-        height:'60vh',
-        width: '100vw',
-        menubar: true,
-        plugins,
-        toolbar,
-      }}
+      init={init}
       onEditorChange={onChange}
       {...rest}
     />
