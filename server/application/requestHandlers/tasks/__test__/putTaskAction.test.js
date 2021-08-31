1c509f556917e5e0cbf867fdcba4496f455cfa05
la <ladams1776@yahoo.com>
2021-08-31 15:59:58 -0400

    Fix putTaskAction test suite We swapped out response handler and so we needed test to reflect these import shifts.

--- server/application/requestHandlers/tasks/__test__/putTaskAction.test.js
+++ server/application/requestHandlers/tasks/__test__/putTaskAction.test.js
@@ -1,12 +1,13 @@
 const TaskService = require("../../../../domain/services/tasks/TaskService");
-const jsonResponse = require("../../jsonResponse");
+const jsonResponse = require("../../apiResponse");
+const { FETCH_TASK_BY_ID_RESPONSE } = require("../../reduxTypes");
 const RequestToTaskDto = require("../assemblers/RequestToTaskDto");
-const updateTaskAction = require("../updateTaskAction");
+const putTaskAction = require("../putTaskAction");
 
-jest.mock("../../jsonResponse");
+jest.mock("../../apiResponse");
 jest.mock("../assemblers/RequestToTaskDto");
 
-describe('updateTaskAction', () => {
+describe('putTaskAction', () => {
     it('should invoke TaskService.updateTask, when action invoked', () => {
         // Arrange
         const req = { id: 'reqID' };
@@ -17,10 +18,10 @@ describe('updateTaskAction', () => {
         TaskService.updateTask = jest.fn();
         
         // Act
-        updateTaskAction(req, resSpy);
+        putTaskAction(req, resSpy);
 
         // Assert
-        expect(jsonResponse).toBeCalledWith(resSpy);
+        expect(jsonResponse).toBeCalledWith(resSpy, FETCH_TASK_BY_ID_RESPONSE);
         expect(RequestToTaskDto).toBeCalledWith(req);
         expect(TaskService.updateTask).toBeCalledWith(incomingDto, resSpy);
     });
