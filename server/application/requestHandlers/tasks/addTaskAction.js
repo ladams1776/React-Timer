2cb647bea0e71a33e59403c9b237b329fe756dc4
la <ladams1776@yahoo.com>
2021-09-03 23:43:21 -0400

    Fix New Task Button So it re-renders the screen with the new note.

--- server/application/requestHandlers/tasks/addTaskAction.js
+++ server/application/requestHandlers/tasks/addTaskAction.js
@@ -1,11 +1,11 @@
 const Task = require('../../../infrastructure/models/Task');
 const hydrate = require('../../../infrastructure/hydrators/hydrate');
- 
+
 //@TODO: Make a Service and Repository
 module.exports = async (req, res) => {
     const m = new Task();
     m.toObject();
     const t = await m.save(hydrate);
 
-    res.jsonp(t);
+    res.jsonp({ items: t, ok: true });
 };
