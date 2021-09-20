30c07292da71653bcbf51d9e207244f9abb1f08c
la <13687739+ladams1776@users.noreply.github.com>
2021-09-19 22:35:17 -0400

    Add validation to putTaskAction

--- server/application/requestHandlers/tasks/putTaskAction.js
+++ server/application/requestHandlers/tasks/putTaskAction.js
@@ -1,10 +1,17 @@
+const { validationResult } = require('express-validator');
 const { FETCH_TASK_BY_ID_RESPONSE } = require('../reduxTypes');
 const TaskService = require('../../../domain/services/tasks/TaskService');
 const apiResponse = require('../apiResponse');
 const RequestToTaskDto = require('./assemblers/RequestToTaskDto');
 
 module.exports = async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        console.log('yes errors', errors)
+        return res.status(400).json({ errors: errors.array() });
+    }
+
     const response = apiResponse(res, FETCH_TASK_BY_ID_RESPONSE);
-    const incomingDto = RequestToTaskDto(req);
+    const incomingDto = RequestToTaskDto(req, res);
     TaskService.updateTask(incomingDto, response);
 };
