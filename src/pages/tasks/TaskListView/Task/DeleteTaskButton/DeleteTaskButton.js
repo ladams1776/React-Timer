c1f972933098598849fd1f7234c66ca10cacc77e
la <13687739+ladams1776@users.noreply.github.com>
2021-10-06 08:39:22 -0400

    Fix delete button now it will refresh the list, when we delete an item. This will allow us to keep the flash message and also clear the stale list.

--- src/pages/tasks/TaskListView/Task/DeleteTaskButton/DeleteTaskButton.js
+++ src/pages/tasks/TaskListView/Task/DeleteTaskButton/DeleteTaskButton.js
@@ -1,20 +1,25 @@
 import React from "react";
 import cn from 'classnames';
 import { fetchApiData } from 'utils';
-import { useBrowserHistory } from 'hooks';
+import { useBrowserHistory, useLoadinSpinnerContext } from 'hooks';
 import useDispatch from './useDispatch';
 import { Button } from "components";
+import useFetchAllTasks from "pages/tasks/hooks/useFetchAllTasks";
 import styles from './DeleteTaskButton.module.css';
 
-const DeleteTaskButton = ({ taskId, isSelected }) => {
+const DeleteTaskButton = ({ taskId, isSelected, setTasks }) => {
   const { push } = useBrowserHistory();
   const dispatch = useDispatch(taskId);
+  const { setIsLoadin } = useLoadinSpinnerContext();
 
   const _deleteClick = async e => {
     const isDeleting = window.confirm("Are you sure you want to delete task?");
     const reload = data => {
       dispatch(data);
-      push('/');
+      //@TODO: Fetch the list of notes now
+      setIsLoadin(true);
+      fetchApiData('tasks', {}, setTasks);
+      setIsLoadin(false);
     };
 
     if (isDeleting) {
--- src/pages/tasks/TaskListView/Task/Task.js
+++ src/pages/tasks/TaskListView/Task/Task.js
@@ -33,7 +33,7 @@ const Task = ({ _id, description, contractId, selectedId, setTasks }) => {
           />
           <span className={styles.taskItemCustomer}>{projectOptionLabel}</span>
         </div>
-        <DeleteTaskButton taskId={_id} isSelected={isSelected} />
+        <DeleteTaskButton taskId={_id} isSelected={isSelected} setTasks={setTasks}/>
       </div>
       <div className={styles.borderContainer}>
         <div className={cn(styles.underBorder, { [styles.underBorderSelected]: isSelected })} />
