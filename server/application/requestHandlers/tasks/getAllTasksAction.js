bb06724907b9601bbde3b6eda42543e7d450b3b5
la <13687739+ladams1776@users.noreply.github.com>
2021-10-10 11:19:22 -0400

    Wire in the new key for getAllTasksAction

--- server/application/requestHandlers/tasks/getAllTasksAction.js
+++ server/application/requestHandlers/tasks/getAllTasksAction.js
@@ -1,6 +1,9 @@
 const TaskService = require('../../../domain/services/tasks/TaskService');
+const apiResponse = require('../apiResponse');
+const { FETCH_TASK_BY_ID_RESPONSE } = require('../reduxTypes');
 
 module.exports = async (req, res) => {
+  const response = apiResponse(res, FETCH_TASK_BY_ID_RESPONSE);
   const tasks = await TaskService.fetchAllTasks();
-  res.jsonp(tasks);
+  response(tasks);
 };
