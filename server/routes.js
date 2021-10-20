adbe026d32f71d1d9ddd4c8d40331a2165afa38f
la <13687739+ladams1776@users.noreply.github.com>
2021-10-19 23:09:37 -0400

    auto format routes

--- server/routes.js
+++ server/routes.js
@@ -21,18 +21,18 @@ module.exports = (app) => {
     // TASKS
     app.get('/api/tasks', getAllTasksAction);
     app.get('/api/task/:id', getTaskByIdAction);
-    app.post('/api/task/', [
-        body('_id').isString().trim().escape(),
-        body('WorkUnit[0].description').isString().trim().escape(),
-        body('date').custom((value) => {
-            const pattern = new RegExp(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
-            if (!pattern.test(value)) {
-                throw new Error('Required in UTC format: YYYY-MM-DDTHH:MM:SS.000Z');
-            }
-            return true;
-        }),
-        body('WorkUnit[0].contractId').isNumeric().trim().escape(),
-    ],
+    app.post('/api/task/',[
+            body('_id').isString().trim().escape(),
+            body('WorkUnit[0].description').isString().trim().escape(),
+            body('date').custom((value) => {
+                const pattern = new RegExp(/\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}/);
+                if (!pattern.test(value)) {
+                    throw new Error('Required in UTC format: YYYY-MM-DDTHH:MM:SS.000Z');
+                }
+                return true;
+            }),
+            body('WorkUnit[0].contractId').isNumeric().trim().escape(),
+        ],
         addTaskAction
     );
     app.put(
