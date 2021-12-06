24971b22d03dcec7ae245dcb6625ffddd31a7df1
la <13687739+ladams1776@users.noreply.github.com>
2021-12-05 20:34:23 -0500

    Fix test for getCurrentDateTimeEstFormat

--- src/utils/formatters/__test__/getCurrentDateTimeEstFormat.test.js
+++ src/utils/formatters/__test__/getCurrentDateTimeEstFormat.test.js
@@ -1,13 +1,11 @@
-import getCurrentDateTimeEstFormat from '../getCurrentDateTimeEstFormat';
+import getCurrentDateTimeEstFormat, { myDatetimeFormat, myTimezone } from '../getCurrentDateTimeEstFormat';
 import moment from 'moment-timezone';
 
 describe('src/utils/__test__/getCurrentDateTimeEstFormat.test.js', () => {
   describe('getCurrentDateTimeEstFormat', () => {
     it('Should format the date and time in EST', () => {
       // Arrange
-      const myTimezone = "America/Toronto";
-      const myDatetimeFormat = "YYYY-MM-DD hh:mm:ss a z";
-      const expected = moment(new Date()).tz(myTimezone).format(myDatetimeFormat);
+      const expected = moment.tz(new Date(), myTimezone).format(myDatetimeFormat);
 
       // Act
       const actual = getCurrentDateTimeEstFormat();
--- src/utils/formatters/getCurrentDateTimeEstFormat.ts
+++ src/utils/formatters/getCurrentDateTimeEstFormat.ts
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone';
 
-const myTimezone = 'America/New_York';
-const myDatetimeFormat = 'YYYY-MM-DDThh:mm:ss';
+export const myTimezone = 'America/New_York';
+export const myDatetimeFormat = 'YYYY-MM-DDThh:mm:ss';
 
 export default function getCurrentDateTimeEstFormat(): string  {
   return moment.tz(new Date(), myTimezone).format(myDatetimeFormat);
