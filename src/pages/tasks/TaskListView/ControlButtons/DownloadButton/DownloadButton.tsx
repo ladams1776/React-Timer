0b9dcf629d8f2d0b072f0f2a42da651b11a2bf9c
la <13687739+ladams1776@users.noreply.github.com>
2021-12-11 21:09:25 -0500

    Move useHandleDownload out to its own cutom hook

--- src/pages/tasks/TaskListView/ControlButtons/DownloadButton/DownloadButton.tsx
+++ src/pages/tasks/TaskListView/ControlButtons/DownloadButton/DownloadButton.tsx
@@ -2,25 +2,23 @@ import React from 'react';
 import cn from 'classnames';
 import { TaskInterface } from 'interfaces/pages/tasks/Task';
 import { Button } from 'components';
-import { writeJsonFile } from './writeJsonFile';
-import useTaskAssembler from './useTaskAssembler';
+import useHandleDownload from './useHandleDownload';
 import styles from './DownloadButton.module.css';
 
 interface DownloadButtonProps {
   tasks: TaskInterface[];
 }
 
-const useHandleDownload = (tasks: TaskInterface[]) => {
-  const assembleTask = useTaskAssembler(tasks);
-  return () => {
-    writeJsonFile(assembleTask());
-  };
-};
-
 const DownloadButton: React.FC<DownloadButtonProps> = (...tasks) => {
   const handleDownload = useHandleDownload(tasks);
 
-  return <>{tasks?.length && <Button title="Download Tasks" type="a" className={cn(styles.buttonDownload, 'glyphicon glyphicon-download-alt')} onClick={handleDownload} />}</>;
+  return (
+    <>
+      {tasks?.length && (
+        <Button data-testid="btn-download" title="Download Tasks" type="a" className={cn(styles.buttonDownload, 'glyphicon glyphicon-download-alt')} onClick={handleDownload} />
+      )}
+    </>
+  );
 };
 
 export default DownloadButton;
