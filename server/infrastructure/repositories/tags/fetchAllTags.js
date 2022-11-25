4bd999d072162190d13af43b6ba7415615acfc3a
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-11-25 00:29:50 -0500

    Modify get all tags RH

--- server/infrastructure/repositories/tags/fetchAllTags.js
+++ server/infrastructure/repositories/tags/fetchAllTags.js
@@ -1,5 +1,5 @@
-const hydrateAndResponse = require("../../hydrators/hydrateAndResponse");
 const Tag = require("../../models/Tag");
 
-module.exports = (res) => Tag.find({}, hydrateAndResponse(res)).sort('-_id');
+module.exports = async () => await Tag.find({})
+    .sort('-_id');
 
