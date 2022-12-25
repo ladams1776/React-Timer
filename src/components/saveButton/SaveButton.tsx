c985f0f4d5f75c9e64115e03fbede500a77386dd
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-12-24 22:49:01 -0500

    Add the form submission to SaveButton

--- src/components/saveButton/SaveButton.tsx
+++ src/components/saveButton/SaveButton.tsx
@@ -3,6 +3,13 @@ import cn from 'classnames';
 import React from 'react';
 import styles from './SaveButton.module.css';
 
-const SaveButton: React.FC = () => <Button type="submit" className={cn(styles.submit, "glyphicon glyphicon-floppy-save")} />
+const SaveButton: React.FC = () => <Button
+    type="submit"
+    className={cn(styles.submit, "glyphicon glyphicon-floppy-save")}
+    onClick={() => {
+        document.getElementById('editForm')
+            ?.dispatchEvent(new Event('submit', { cancelable: true, bubbles: true }))
+    }}
+/>
 
 export default SaveButton;
