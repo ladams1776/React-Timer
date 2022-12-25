524d01a926c2684292aad42fd86be922c9d6ff8d
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-12-24 22:48:25 -0500

    Add form to add tag form

--- src/pages/tags/AddTagForm/AddTagForm.js
+++ src/pages/tags/AddTagForm/AddTagForm.js
@@ -1,48 +1,51 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import cn from 'classnames';
 import { Form, Field } from 'react-final-form'
 import { Button, TextAreaAdapter } from 'components';
 import { useSetCurrentLocation } from 'hooks'
 import useFormSetup from './useFormSetup';
 import useFetchTagByIdDispatch from './useFetchTagByIdDispatch';
-import styles from './AddTagForm.module.css';
 import useTagByIdSelector from 'redux/selectors/useTagById';
 import SaveButton from 'components/saveButton/SaveButton';
 import TopBar from 'components/topBar/TopBar';
 
+import styles from './AddTagForm.module.css';
+
 const AddTagePage = ({ tagId }) => {
-    useSetCurrentLocation(`/tag/${tagId}`);
+    // useSetCurrentLocation(`/tag/${tagId}`);
     useFetchTagByIdDispatch(tagId);
     const tag = useTagByIdSelector();
-    const { onSubmit } = useFormSetup(tagId);
+    const onSubmit = useFormSetup();
 
     return (
         <div className={styles.container}>
-            <TopBar>
-                <SaveButton />
-            </TopBar>
             <Form
                 onSubmit={onSubmit}
                 initialValues={tag}
                 render={({ handleSubmit }) => {
                     return (
                         <form
+                            id="addForm"
                             data-test-id="form"
                             className={styles.form}
                             onSubmit={handleSubmit}>
-                            <Field
-                                name="name"
-                                component="input"
-                                className={styles.name}
-                            />
-                            <Field name="description" component={TextAreaAdapter} />
-
-                            <div className={styles.submitContainer}>
-                                <Button type="submit"
-                                    className={cn("btn", "btn-primary", styles.submit)}
-                                    data-test-id="addTagPageSubmit"
-                                    value="Submit Form" />
-                            </div>
+                            <TopBar>
+                                <SaveButton />
+                            </TopBar>
+                            <fieldset classname={styles.formField}>
+                                <label htmlFor="name">Name</label>
+                                <Field
+                                    name="name"
+                                    component="input"
+                                    className={styles.name}
+                                />
+                            </fieldset>
+                            <fieldset>
+                                <Field
+                                    name="description"
+                                    id="description"
+                                    component={TextAreaAdapter} />
+                            </fieldset>
                         </form>
                     );
                 }}
