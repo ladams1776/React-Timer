8cf75b3131bd5016dd48cb69e5a1256828d16216
daedalus1215 <13687739+daedalus1215@users.noreply.github.com>
2022-12-24 22:48:35 -0500

    Break up lines in Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,19 +5,24 @@ import styles from './Button.module.css';
 
 interface ButtonProps {
   className?: string;
-  onClick?: (e?:any) => void;
+  onClick?: (e?: any) => void;
   value?: string | number;
   children?: React.FC;
   testid?: string;
-  title?:string;
+  title?: string;
   type?: string;
   disabled?: boolean
 }
 
-const Button :React.FC<ButtonProps> = ({ className, onClick, value, children, testid, title, type, disabled, ...rest }) => {
-  const clickCallback = useRippleEffect('button', onClick || (() => {}));
+const Button: React.FC<ButtonProps> = ({ className, onClick, value, children, testid, title, type, disabled, ...rest }) => {
+  const clickCallback = useRippleEffect('button', onClick || (() => { }));
   return (
-    <button className={cn('btn', 'btn-default', 'navbar-btn', styles.baseBtn, className)} onClick={clickCallback} data-testid={testid} disabled={disabled} {...rest}>
+    <button
+      className={cn('btn', 'btn-default', 'navbar-btn', styles.baseBtn, className)}
+      onClick={clickCallback}
+      data-testid={testid}
+      disabled={disabled}
+      {...rest}>
       {children}
       {value}
     </button>
